0d78a86fd5ccb5fa762898e2076a240efa031ef3
razannael <nailrazan40@gmail.com>
2024-04-07 18:39:03 +0300

    Get Call Functions

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,8 +1,46 @@
 import { useGetCalls } from '@/hooks/useGetCalls'
-import React from 'react'
+import { CallRecording } from '@stream-io/video-react-sdk';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const CallList = ({type}:{type: 'ended' | 'upcoming' | 'recordings'}) => {
     const {endedCalls, upcomingCalls, callRecordings, isLoading} = useGetCalls();
+    const router = useRouter();
+    const [recordings, setRecordings] = useState<CallRecording[]>([]);
+    const getCalls = () => {
+        switch (type) {
+            case 'ended':
+                return endedCalls;
+                break;
+            case 'upcoming':
+                return upcomingCalls;
+                break;
+            case 'recordings':
+                return recordings;
+                break;
+            default:
+                return [];
+                break;
+        }
+    };
+ 
+    const getNoCallMessage = () => {
+        switch (type) {
+            case 'ended':
+                return 'No previous calls';
+                break;
+            case 'upcoming':
+                return 'No upcoming calls';
+                break;
+            case 'recordings':
+                return 'No recordings';
+                break;
+            default:
+                return '';
+                break;
+        }
+    };
+
     return (
     <div>CallList</div>
   )
