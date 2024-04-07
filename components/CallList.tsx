d6a2d967260cc09f94738fdaa20fff9461f14f93
razannael <nailrazan40@gmail.com>
2024-04-07 18:21:32 +0300

    Get Calls

---
+++ components/CallList.tsx
@@ -0,0 +1,11 @@
+import { useGetCalls } from '@/hooks/useGetCalls'
+import React from 'react'
+
+const CallList = ({type}:{type: 'ended' | 'upcoming' | 'recordings'}) => {
+    const {endedCalls, upcomingCalls, callRecordings, isLoading} = useGetCalls();
+    return (
+    <div>CallList</div>
+  )
+}
+
+export default CallList
